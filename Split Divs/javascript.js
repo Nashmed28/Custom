8a4a1a0e4dd8b112db5970cf8c259f8b6dea26a2
Nabib Ahmed <nahmed3536@gmail.com>
2016-08-01 21:47:51 -0400

    First commit: split divs looks solid for holy grail here

---
+++ Split Divs/javascript.js
@@ -0,0 +1,19 @@
+// // function my () {
+// // 	alert('hi');
+// // }
+
+// $('#slider1').draggable({
+//     axis: "x"
+// });
+
+// $('#col3').on('click', function() {
+// 	alert('hi');
+// 	// var x1 = x2 = $(".slider").position().top;
+// 	// var y1 = $(".slider").position().left;
+// 	// var y2 = ($(window).width() + $(".slider").position().left);
+// 	// $(".slider").draggable('option', 'containment', [x1, y1, x2, y2]);
+// });
+
+document.getElementById('slider1').onmousedown = function () {
+	alert('click');
+}
